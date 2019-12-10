fddee88b8dc161ce920e4db32af3eaea8a5beb65
Madhu Venugopal <madhu@docker.com>
2019-12-10 08:11:09 -0800

    Second attempt on Puzzle 10 using atan2 method of calcuating angles

--- 2019/bruteforce/puzzle10-1.go
+++ 2019/bruteforce/puzzle10-1.go
@@ -3,13 +3,14 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math"
 	"os"
 )
 
-var astroidMap [][]rune
+var astroidMap []coordinate
 
-var EMPTY = rune(46)
-var LOCATED = rune(35)
+var EMPTY = 46   // .
+var LOCATED = 35 // #
 var totalAstroids int
 var totalEmpty int
 
@@ -17,175 +18,67 @@ func main() {
 	f, _ := os.Open("puzzle10.input")
 	scanner := bufio.NewScanner(f)
 
+	y := 0
 	for scanner.Scan() {
 		line := scanner.Text()
-		mapAstroid(line)
-	}
-	for _, aArray := range astroidMap {
-		for _, a := range aArray {
-			if a == LOCATED {
-				totalAstroids++
-			} else {
-				totalEmpty++
+		for x, hit := range line {
+			if hit == rune(LOCATED) {
+				astroidMap = append(astroidMap, coordinate{x, y})
 			}
 		}
+		y++
 	}
+
 	fmt.Printf("Final Answer : %d", findBestNumbers())
 }
 
 func findBestNumbers() int {
 	best := 0
-	for x, aArray := range astroidMap {
-		for y, a := range aArray {
-			if a == LOCATED {
-				n := numAstroidsInLOS(x, y)
-				if n > best {
-					best = n
+	for _, curr := range astroidMap {
+		angles := make(map[float64]coordinate)
+		c := 0
+		for _, running := range astroidMap {
+			if curr != running {
+				angle := curr.angle(running)
+				if _, ok := angles[angle]; !ok {
+					angles[angle] = running
+					c++
 				}
 			}
+		}
+		if c > best {
+			best = c
 		}
 	}
 	return best
 }
 
-func numAstroidsInLOS(x int, y int) int {
-	numBlocked := 0
-	blocked := false
-	//EAST
-	for e := x + 1; e < len(astroidMap); e++ {
-		if astroidMap[e][y] == LOCATED {
-			if !blocked {
-				blocked = true
-			} else {
-				numBlocked++
-			}
-		}
-	}
-	blocked = false
-	//WEST
-	for w := x - 1; w >= 0; w-- {
-		if astroidMap[w][y] == LOCATED {
-			if !blocked {
-				blocked = true
-			} else {
-				numBlocked++
-			}
-		}
-	}
-	blocked = false
-	//NORTH
-	for n := y - 1; n >= 0; n-- {
-		if astroidMap[x][n] == LOCATED {
-			if !blocked {
-				blocked = true
-			} else {
-				numBlocked++
-			}
-		}
-	}
-	blocked = false
-	//SOUTH
-	for s := y + 1; s < len(astroidMap[x]); s++ {
-		if astroidMap[x][s] == LOCATED {
-			if !blocked {
-				blocked = true
-			} else {
-				numBlocked++
-			}
-		}
-	}
-
-	var tx int
-	//NE
-	//Immediate East row wont be blocking ... hence starting with x+2
-	for e := x + 2; e < len(astroidMap); e++ {
-		//Immediate North row wont be blocking ... hence starting with y-2
-		for n := y - 2; n >= 0; n-- {
-			if astroidMap[e][n] == LOCATED {
-				if (e-x)%(y-n) == 0 {
-					tx = e
-					for s := n + 1; s >= y-1; s++ {
-						tx = tx - (e-x)/(y-n)
-						if astroidMap[tx][s] == LOCATED {
-							numBlocked++
-							break
-						}
-
-					}
-
-				}
-			}
-		}
-	}
-
-	//NW
-	//Immediate West row wont be blocking ... hence starting with x-2
-	for w := x - 2; w >= 0; w-- {
-		//Immediate North row wont be blocking ... hence starting with y-2
-		for n := y - 2; n >= 0; n-- {
-			if astroidMap[w][n] == LOCATED {
-				if (y-n)%(x-w) == 0 {
-					tx = w
-					for s := n + 1; s >= y-1; s++ {
-						tx = tx + (y-n)/(x-w)
-						if astroidMap[tx][s] == LOCATED {
-							numBlocked++
-							break
-						}
-
-					}
-
-				}
-			}
-		}
-	}
-
-	//SE
-	//Immediate East row wont be blocking ... hence starting with x+2
-	for e := x + 2; e < len(astroidMap); e++ {
-		//Immediate South row wont be blocking ... hence starting with y+2
-		for s := y + 2; s < len(astroidMap[e]); s++ {
-			if astroidMap[e][s] == LOCATED {
-				if e%s == 0 {
-					tx = e
-					for n := s - 1; n < y-1; n-- {
-						tx = tx - e/s
-						if astroidMap[tx][n] == LOCATED {
-							numBlocked++
-							break
-						}
-
-					}
-
-				}
-			}
-		}
-	}
-	//SW
-	//Immediate West row wont be blocking ... hence starting with x+2
-	for w := x - 2; w >= 0; w-- {
-		//Immediate South row wont be blocking ... hence starting with y+2
-		for s := y + 2; s < len(astroidMap[w]); s++ {
-			if astroidMap[w][s] == LOCATED {
-				if (s-y)%(x-w) == 0 {
-					tx = w
-					for n := s - 1; n < y-1; n-- {
-						tx = tx + (s-y)/(x-w)
-						if astroidMap[tx][n] == LOCATED {
-							numBlocked++
-							break
-						}
-
-					}
-
-				}
-			}
-		}
-	}
-	fmt.Printf("(%d - %d = %d) ", totalAstroids, numBlocked, totalAstroids-numBlocked)
-	return totalAstroids - numBlocked
+type coordinate struct {
+	x, y int
 }
 
-func mapAstroid(coordinates string) {
-	astroidMap = append(astroidMap, []rune(coordinates))
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func (c coordinate) distance(t coordinate) int {
+	return abs(c.x-t.x) + abs(c.y-t.y)
+}
+
+func (c coordinate) angle(t coordinate) float64 {
+	a := math.Atan2(float64(t.x-c.x), float64(c.y-t.y)) * 180 / math.Pi
+	if a < 0 {
+		a = 360 + a
+	}
+	return a
 }
